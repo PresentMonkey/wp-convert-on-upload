f091a42de93eb9e8cc651228abbf184c0092912d
Patrick G <gscience213@gmail.com>
2022-07-11 09:09:39 -0400

    "multithreading"

--- wp-convert-on-upload.php
+++ wp-convert-on-upload.php
@@ -20,25 +20,37 @@ License: MIT
 
 */
 
+if ( ! function_exists( 'wp_crop_image' ) ) {
+  include( ABSPATH . 'wp-admin/includes/image.php' );
+}
+
 add_action('wp_handle_upload', 'custom_upload_filter');
 function custom_upload_filter( $imagedata ) {
     $filetype = wp_check_filetype($imagedata['file']);
     if($filetype['type'] == 'image/heic'){
         $image_path = $imagedata['file'];
-        $im = new Imagick();
-        $im->readImage($image_path);
-        $im->setImageFormat("jpg");
-        file_put_contents($image_path, $im);
         $newPath = preg_replace("/\.heic$/", ".jpg", $image_path);
         $newUrl = preg_replace("/\.heic$/", ".jpg", $imagedata['url']);
-        rename($image_path, $newPath);
         $imagedata['file'] = $newPath;
         $imagedata['url'] = $newUrl;
         $imagedata['type'] = 'image/jpeg';
-
+        //we do the actual convertion in an "event" to not block the main thread/loop
+        wp_schedule_single_event(time(), 'convert_file', array($image_path, $newPath, $newUrl));
         return $imagedata;
     }
     else{
         return $imagedata;
     }
+}
+
+add_action('convert_file', 'convert', 10, 3);
+function convert($oldpath, $newpath, $url){
+        $im = new Imagick();
+        $im->readImage($oldpath);
+        $im->setImageFormat("jpg");
+        file_put_contents($oldpath, $im);
+        rename($oldpath, $newpath);
+        $image_id = attachment_url_to_postid($url);
+        $metadata = wp_generate_attachment_metadata($image_id, $newpath);
+        wp_update_attachment_metadata($image_id, $metadata);
 }
